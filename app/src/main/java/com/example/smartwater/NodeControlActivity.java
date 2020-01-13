a22abae58490a0baa1ba8d4409e26694082f28f5
rishithSadashiv <rishith.1si16ec078@gmail.com>
2020-01-14 03:35:26 +0530

    data retrieving feature added

--- app/src/main/java/com/example/smartwater/NodeControlActivity.java
+++ app/src/main/java/com/example/smartwater/NodeControlActivity.java
@@ -1,15 +1,26 @@
 package com.example.smartwater;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class NodeControlActivity extends AppCompatActivity {
 
     Intent intent;
-    TextView textView;
+    TextView id, latitude, longitude, location, boreDrillDate, motorInstallationDate, borewellDepth, casingLength, pipeInLength, cableLength;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference databaseNodes = database.getReference("Nodes");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +30,49 @@ public class NodeControlActivity extends AppCompatActivity {
         intent = getIntent();
         String uid = intent.getStringExtra("UID");
 
-        textView = findViewById(R.id.textViewNodeControlActivity);
-        textView.setText(uid);
+        id = findViewById(R.id.textViewId);
+        id.setText(uid);
+
+        latitude = findViewById(R.id.textViewlatitude);
+        longitude = findViewById(R.id.textViewLongitude);
+        location = findViewById(R.id.textViewLocation);
+        boreDrillDate = findViewById(R.id.textViewBoreDrillDate);
+        motorInstallationDate = findViewById(R.id.textViewMotorInstallationDate);
+        borewellDepth = findViewById(R.id.textViewBorewellDepth);
+        casingLength = findViewById(R.id.textViewCasingLength);
+        pipeInLength = findViewById(R.id.textViewPipeInLength);
+        cableLength = findViewById(R.id.textViewCableLength);
+
+        databaseNodes.child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String latitud = dataSnapshot.child("lattitude").getValue().toString();
+                String longitud = dataSnapshot.child("longitude").getValue().toString();
+                String locatio = dataSnapshot.child("location").getValue().toString();
+                String boreDrillDat = dataSnapshot.child("drillDate").getValue().toString();
+                String motorInstallationDat = dataSnapshot.child("motorInstallationDate").getValue().toString();
+                String borewellDept = dataSnapshot.child("depth").getValue().toString();
+                String casingLengt = dataSnapshot.child("casingLength").getValue().toString();
+                String pipeInLengt = dataSnapshot.child("pipeInLength").getValue().toString();
+                String cableLengt = dataSnapshot.child("cableLength").getValue().toString();
+
+                latitude.setText(latitud);
+                longitude.setText(longitud);
+                location.setText(locatio);
+                boreDrillDate.setText(boreDrillDat);
+                motorInstallationDate.setText(motorInstallationDat);
+                borewellDepth.setText(borewellDept);
+                casingLength.setText(casingLengt);
+                pipeInLength.setText(pipeInLengt);
+                cableLength.setText(cableLengt);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
     }
